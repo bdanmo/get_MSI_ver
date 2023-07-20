7be7956122e0597b643d2763024aa0f65c45ec42
Brendan Moran <b.daniel.moran@gmail.com>
2023-07-20 13:42:21 -0400

    and now for something completely different

--- get_MSI_ver.ps1
+++ get_MSI_ver.ps1
@@ -1,17 +1,21 @@
-$pathToMSI = Read-Host -Prompt "Enter path to MSI file"
-
-$windowsInstaller = New-Object -com WindowsInstaller.Installer
-$database = $windowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $Null, $windowsInstaller, @($pathToMSI, 0))
-
-$query = "SELECT `Value` FROM `Property` WHERE `Property` = 'ProductVersion'"
-$view = $database.GetType().InvokeMember("OpenView", "InvokeMethod", $Null, $database, ($query))
-
-$view.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $view, $Null)
-
-$record = $view.GetType().InvokeMember("Fetch", "InvokeMethod", $Null, $view, $Null)
-$version = $record.GetType().InvokeMember("StringData", "GetProperty", $Null, $record, 1)
-
-write-host $version
-
-Read-Host -Prompt "Press Enter to exit"
+param (
+    [parameter(Mandatory=$true)] 
+    [ValidateNotNullOrEmpty()] 
+        [System.IO.FileInfo] $MSIPATH
+) 
+if (!(Test-Path $MSIPATH.FullName)) { 
+    throw "File '{0}' does not exist" -f $MSIPATH.FullName 
+} 
+try { 
+    $WindowsInstaller = New-Object -com WindowsInstaller.Installer 
+    $Database = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $Null, $WindowsInstaller, @($MSIPATH.FullName, 0)) 
+    $Query = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
+    $View = $database.GetType().InvokeMember("OpenView", "InvokeMethod", $Null, $Database, ($Query)) 
+    $View.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $View, $Null) | Out-Null
+    $Record = $View.GetType().InvokeMember( "Fetch", "InvokeMethod", $Null, $View, $Null ) 
+    $Version = $Record.GetType().InvokeMember( "StringData", "GetProperty", $Null, $Record, 1 ) 
+    return $Version
+} catch { 
+    throw "Failed to get MSI file version: {0}." -f $_
+}    
 
